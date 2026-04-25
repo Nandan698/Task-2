78e5d320a77b22c0e772d714bea6b92c780a5aaf
Amal Nandan Manoj <amanoj4@stu.vistula.edu.pl>
2026-04-25 15:48:34 +0200

    Task 2B - Swagger UI - Finished

--- src/main/java/pl/edu/vistula/task_2/Task2Application.java
+++ src/main/java/pl/edu/vistula/task_2/Task2Application.java
@@ -2,8 +2,11 @@ package pl.edu.vistula.task_2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Products API for Vistula",version = "1.0",description = "Products Information"))
 public class Task2Application {
 
     public static void main(String[] args) {
